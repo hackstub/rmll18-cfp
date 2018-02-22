fb06e69318192c101fa4f44003dc55718e50e9b2
Axolotle <nchesnais@mailoo.org>
2018-02-22 16:03:11 +0100

    Setting same height for every <p> layers

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+
+'use strict';
+
 var trans = document.getElementById('trans');
 var slider = document.getElementById('slider');
+setSourceBlockHeight();
 
 slider.addEventListener('mousedown', sliderListener);
 window.addEventListener('scroll', (e) => {
-    trans.scrollTop = e.pageY;
+    trans.scrollTop = e.pageY != undefined ? e.pageY : document.body.scrollTop;
+});
+window.addEventListener('resize', (e) => {
+    changeHeight();
+    setSourceBlockHeight();
 });
 
 function sliderListener(e) {
@@ -20,9 +28,19 @@ function sliderListener(e) {
 }
 
 function changeHeight(e) {
-    var y = e.clientY;
+    var y = e != undefined ? e.clientY : window.innerHeight - 50;
     if (y >= 0) {
         slider.style.bottom = window.innerHeight - y + 'px';
         trans.style.height = y + 'px';
+    }
+}
+
+function setSourceBlockHeight() {
+    var ps = document.getElementById('source').getElementsByTagName('P');
+    var pt = trans.getElementsByTagName('P');
+
+    for (var i = 0; i < ps.length; i++) {
+        var height = pt[i].offsetHeight;
+        ps[i].style.height = height - 2 + 'px';
     }
 }
