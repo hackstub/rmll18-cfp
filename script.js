bed615415d0bf2cc1859e5ac29816993790bb952
Axolotle <nchesnais@mailoo.org>
2018-03-08 15:06:08 +0100

    Adapting menu displayer and hiding rmll mention for small screen

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// 'use strict';
+'use strict';
 
 var trad = document.getElementById('trad');
-var source = document.getElementById('source')
+var source = document.getElementById('source');
 var slider = document.getElementById('slider');
-var littleTitle = document.getElementsByTagName('NAV')[0];
+var rmllMention = document.getElementById('rmll');
 var h, w, portrait;
+
 window.onload = init;
+window.addEventListener('resize', update);
+
+document.getElementById('button').onclick = displayMenu;
 
 slider.addEventListener('mousedown', sliderListener);
 slider.addEventListener('touchstart', sliderTouchListener);
 
-window.addEventListener('scroll', function (e) {
-    trad.scrollTop = e.pageY || document.body.scrollTop || window.scrollY;
-}, { passive: true });
-
-window.addEventListener('resize', update);
-document.getElementById('button').onclick = displayMenu;
+window.addEventListener('scroll', adaptScroll, { passive: true });
 
 // 'Read only' button, totally hide the source
 document.getElementById('read').onclick = function () {
@@ -24,7 +23,6 @@ document.getElementById('read').onclick = function () {
 }
 
 function init() {
-    // FIXME Temporarily duplicate nodes
     duplicateNodes();
     // setup the needed css to display the source here in case of disabled js
     // or for very old browsers
@@ -41,10 +39,10 @@ function init() {
 function update() {
     h = window.innerHeight;
     w = window.innerWidth;
-    portrait = (h / w) > 1;
+    portrait = h > w;
 
     if (portrait) {
-        changeHeight(h / 1.3);
+        changeHeight(h / 1.15);
     } else {
         changeHeight(h / 1.4);
     }
@@ -54,22 +52,21 @@ function update() {
 function displayMenu() {
     function hideMenu() {
         menu.style.display = 'none';
-        littleTitle.children[2].style.display = 'none';
-        if (portrait || w <= 1024) {
+        if (portrait) {
             setTimeout(function () {
-                window.scrollBy(0, -100)
+                window.scrollBy(0, -80)
             }, 10);
         }
         menu.removeEventListener('click', hideMenu);
     }
 
     var menu = document.getElementById('menu');
-    if (menu.style.display == 'block') {
+    if (menu.style.display === 'block') {
         hideMenu();
     }
     else {
-        menu.style.display = "block";
-        littleTitle.children[2].style.display = 'block';
+        menu.style.display = 'block';
+        rmllMention.style.display = 'block';
         menu.addEventListener('click', hideMenu);
     }
 }
@@ -79,7 +76,7 @@ function sliderListener(e) {
         changeHeight(event.clientY)
     }
     function removeListeners(event) {
-        changeHeight(event.clientY);
+        getY(event);
         window.removeEventListener("mousemove", getY);
         window.removeEventListener("mouseup", removeListeners);
     }
@@ -102,6 +99,22 @@ function sliderTouchListener(e) {
     getY(e);
     window.addEventListener("touchmove", getY);
     window.addEventListener("touchend", removeListeners);
+}
+
+function adaptScroll(e) {
+    var scroll = e.pageY || document.body.scrollTop || window.scrollY;
+    // Adapt scroll value changes to the fixed 'trad' div
+    trad.scrollTop = scroll;
+
+    // Hide the rmll mention for small screens after scrolling the splash screen
+    var rmllDisplay = rmllMention.style.display;
+    var smallScreen = portrait && w < 768 && scroll > h;
+    if (smallScreen && (rmllDisplay === 'block' || rmllDisplay === '')) {
+        rmllMention.style.display = 'none';
+    }
+    else if (rmllDisplay === 'none' && scroll <= h) {
+        rmllMention.style.display = 'block';
+    }
 }
 
 function changeHeight(y) {
@@ -145,6 +158,5 @@ function setSourceBlockHeight() {
 
 function duplicateNodes() {
     var main = document.getElementsByTagName('SECTION')[2].cloneNode(true);
-    var source = document.getElementById('source');
     source.appendChild(main);
 }
